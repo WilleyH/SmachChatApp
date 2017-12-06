55c7c4b88261a3f58470946659c955b8bbf86c93
Casper Holak <33180890+WilleyH@users.noreply.github.com>
2017-12-06 23:31:46 +0100

    sign up view

---
+++ app/src/main/java/com/kholak/smack/CreateUserActivity.kt
@@ -0,0 +1,26 @@
+package com.kholak.smack
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+
+
+        fun generateUserAvatar(view: View) {
+
+        }
+
+        fun createBackgroundColourBtnClicked(view: View) {
+
+        }
+
+        fun createCreateUserBtnClicked(view: View) {
+
+        }
+    }
+}
--- app/src/main/java/com/kholak/smack/LoginActivity.kt
+++ app/src/main/java/com/kholak/smack/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.kholak.smack
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -16,6 +17,7 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun loginLoginBtnClicked(view: View) {
-
+        val createUserIntent = Intent(this, CreateUserActivity :: class.java)
+        startActivity(createUserIntent)
     }
 }
