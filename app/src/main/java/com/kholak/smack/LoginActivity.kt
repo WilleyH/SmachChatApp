00ce938cf99e955bb81bc285bfa20d6e15ab5995
Casper Holak <33180890+WilleyH@users.noreply.github.com>
2017-12-06 23:18:54 +0100

    login view

---
+++ app/src/main/java/com/kholak/smack/LoginActivity.kt
@@ -0,0 +1,21 @@
+package com.kholak.smack
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun loginCreateUserBtnClicked(view: View) {
+
+    }
+
+    fun loginLoginBtnClicked(view: View) {
+
+    }
+}
--- app/src/main/java/com/kholak/smack/MainActivity.kt
+++ app/src/main/java/com/kholak/smack/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.kholak.smack
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
@@ -34,7 +35,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun loginBtnNavClicked(view: View) {
-
+        val loginIntent = Intent(this, LoginActivity :: class.java)
+        startActivity(loginIntent)
     }
 
     fun addChannelClicked(view: View) {
