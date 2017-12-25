58aacc37ff0d35b2376f14acf08344727c46e6ec
Casper Holak <33180890+WilleyH@users.noreply.github.com>
2017-12-25 11:46:27 +0100

    get channnels added

---
+++ app/src/main/java/com/kholak/smack/Model/Channel.kt
@@ -0,0 +1,7 @@
+package com.kholak.smack.Model
+
+/**
+ * Created by nsilska on 2017-12-17.
+ */
+class Channel {
+}
---
+++ app/src/main/java/com/kholak/smack/Services/MessageService.kt
@@ -0,0 +1,7 @@
+package com.kholak.smack.Services
+
+/**
+ * Created by nsilska on 2017-12-17.
+ */
+object MessageService {
+}
